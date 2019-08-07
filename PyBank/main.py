05ab3439523247d0414673e39d20f725361117fd
PadillaM5 <michael.a.padilla1@gmail.com>
2019-08-07 18:34:47 -0400

    .

--- PyBank/main.py
+++ PyBank/main.py
@@ -11,7 +11,8 @@ totalchange = 0
 totalchangelst = []
 xmonth = ''
 ymonth = ''
-
+x= 0
+y = 0
 
 with open(csvpath, newline='') as nfile:
     csvreader = csv.reader(nfile, delimiter=',')
@@ -20,22 +21,24 @@ with open(csvpath, newline='') as nfile:
     for row in csvreader:
         monthscount += 1
         totalnet += int(row[1])
-        x = int(row[1])
-        y = int(row[1])
+
 
      
         totalchange = int(row[1]) - lastrow
-        lastrow = int(row[1])   
         totalchangelst.append(totalchange)
+        
+        lastrow = int(row[1])   
 
 
-        if int(row[1]) > x:
+
+        if totalchange > x:
             x = int(row[1])
             xmonth = str(row[0])
-
-        if int(row[1]) < y:
+            xy = totalchange
+        if totalchange < y:
             y = int(row[1])
             ymonth = str(row[0])
+            yx = totalchange
     
     
     avgchange = sum(totalchangelst)/len(totalchangelst)
@@ -50,9 +53,19 @@ print(f"---------------------------")
 print(f"Total Months: {monthscount}")
 print(f"Total: ${totalnet}")
 print(f"Average Change: ${avgchange}")
-print(f"Greatest Increase in Profits:,{xmonth} (${highest})")
-print(f"Greatest Decrease in Profits:,{ymonth} (${lowest})")
+print(f"Greatest Increase in Profits:,{xmonth} (${xy})")
+print(f"Greatest Decrease in Profits:,{ymonth} (${yx})")
 
 
 
+output_file = os.path.join('..', 'PyBank', 'outcome.text')
 
+
+# with open(output_file, 'w',) as txtfile:
+#     txtfile.write(f"Financial Analysis\n")
+#     txtfile.write(f"---------------------------\n")
+#     txtfile.write(f"Total Months: {monthscount}\n")
+#     txtfile.write(f"Total: ${totalnet}\n")
+#     txtfile.write(f"Average Change: ${avgchange}\n")
+#     txtfile.write(f"Greatest Increase in Profits:, {xmonth}, (${x})\n")
+#     txtfile.write(f"Greatest Decrease in Profits:,{ymonth} (${y})\n")
